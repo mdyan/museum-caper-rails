dd4747c489fb87dd617f73c44c0b5aadc3a28ab6
Michael Yan <michaelyan161@gmail.com>
2012-06-29 19:09:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuseumCaperOnline::Application.config.secret_token = '0d578b20018483060356bd90e9ee31469a78ffab0e9d711d5591920e5cc37c09448d598bf3062d0fed81ca6ad57c135c8eda88bdcd07045294a3c6972f2276af'
