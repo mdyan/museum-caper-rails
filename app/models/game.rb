44488ab5891e837b354ff3b71deeace0d0beb369
Michael Yan <michaelyan161@gmail.com>
2012-07-02 11:53:13 -0400

    fixed game model so that it compiles lol

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
   attr_accessible :finished_at, :last_move_at, :started_at
-  belongs_to :players, class: :User
+  has_and_belongs_to_many :users
   has_many :pieces
 end
